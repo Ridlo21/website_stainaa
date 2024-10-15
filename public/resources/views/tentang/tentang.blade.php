2df65b6516fe45cb0bd542c1c855a976fca4aabd
Ridlo21 <inurrie99@gmail.com>
2024-10-16 02:07:51 +0700

    tentang

--- public/resources/views/tentang/tentang.blade.php
+++ public/resources/views/tentang/tentang.blade.php
@@ -10,21 +10,21 @@ Tentang
 <section class="ftco-intro ftco-no-pt ftco-no-pb img" style="background-image: url(plugin/images/bg_3.jpg);">
     <div class="overlay"></div>
     <div class="container-xl py-5">
-      <div class="row justify-content-center">
-        <div class="col-lg-10 col-xl-8">
-          <div class="row">
-            <div class="col-md-8 d-flex align-items-center">
-              <div class="mt-5">
-                <h1 class="mb-4"></h1>
-              </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-10 col-xl-8">
+                <div class="row">
+                    <div class="col-md-8 d-flex align-items-center">
+                        <div class="mt-5">
+                            <h1 class="mb-4"></h1>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </section>
+</section>
 
-<section class="ftco-section testimony-section bg-light py-5" >
+<section class="ftco-section testimony-section bg-light py-5">
     <div class="container-xl">
         <div class="row">
             <div class="col-md-12" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">
@@ -50,7 +50,7 @@ Tentang
                             <div class="text">
                                 <p class="mb-4 msg">SEJARAH</p>
                                 <div class="d-flex align-items-center">
-                                    <div class="user-img" style="background-image: url(plugin/images/person_2.jpg)"></div>
+                                    <div class="user-img shadow-lg" style="background-image: url(plugin/images/p.png)"></div>
                                     <div class="pl-3 tx">
                                         <p class="name">HISTORICAL</p>
                                         <span class="position">of Stainaa</span>
@@ -65,7 +65,7 @@ Tentang
                             <div class="text">
                                 <p class="mb-4 msg">PROFIL PIMPINAN</p>
                                 <div class="d-flex align-items-center">
-                                    <div class="user-img" style="background-image: url(plugin/images/person_2.jpg)"></div>
+                                    <div class="user-img shadow-lg" style="background-image: url(plugin/images/h.png)"></div>
                                     <div class="pl-3 tx">
                                         <p class="name">LEADERS</p>
                                         <span class="position">of Stainaa</span>
