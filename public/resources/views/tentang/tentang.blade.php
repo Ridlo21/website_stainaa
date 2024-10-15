11bf555fa2ba48174720363c8a7846c53850fdce
oles <muklek99@gmail.com>
2024-10-16 01:31:19 +0700

    ok

--- public/resources/views/tentang/tentang.blade.php
+++ public/resources/views/tentang/tentang.blade.php
@@ -7,37 +7,38 @@ Tentang
 @section('konten')
 
 
+<section class="ftco-intro ftco-no-pt ftco-no-pb img" style="background-image: url(plugin/images/bg_3.jpg);">
+    <div class="overlay"></div>
+    <div class="container-xl py-5">
+      <div class="row justify-content-center">
+        <div class="col-lg-10 col-xl-8">
+          <div class="row">
+            <div class="col-md-8 d-flex align-items-center">
+              <div class="mt-5">
+                <h1 class="mb-4"></h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
-<section class="ftco-section testimony-section bg-light">
+<section class="ftco-section testimony-section bg-light py-5" >
     <div class="container-xl">
         <div class="row">
             <div class="col-md-12" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="100">
                 <div class="carousel-testimony">
                     <div class="item">
                         <div class="testimony-wrap">
-                            <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-check"></div>
-                            <div class="text">
-                                <p class="mb-4 msg">PROFIL PEMIMPIN.</p>
-                                <div class="d-flex align-items-center">
-                                    <div class="user-img" style="background-image: url(plugin/images/person_1.jpg)"></div>
-                                    <div class="pl-3 tx">
-                                        <p class="name">Roger Scott</p>
-                                        <span class="position">Marketing Manager</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="testimony-wrap">
                             <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-quote-left"></div>
                             <div class="text">
-                                <p class="mb-4 msg">MOTTO</p>
+                                <p class="mb-4 msg ">VISI, MISI, MOTTO</p>
                                 <div class="d-flex align-items-center">
-                                    <div class="user-img" style="background-image: url(plugin/images/person_2.jpg)"></div>
+                                    <div class="user-img shadow-lg" style="background-image: url(plugin/images/v.png)"></div>
                                     <div class="pl-3 tx">
-                                        <p class="name">Roger Scott</p>
-                                        <span class="position">Marketing Manager</span>
+                                        <p class="name">VISION</p>
+                                        <span class="position">about stainna</span>
                                     </div>
                                 </div>
                             </div>
@@ -49,10 +50,10 @@ Tentang
                             <div class="text">
                                 <p class="mb-4 msg">SEJARAH</p>
                                 <div class="d-flex align-items-center">
-                                    <div class="user-img" style="background-image: url(plugin/images/person_3.jpg)"></div>
+                                    <div class="user-img" style="background-image: url(plugin/images/person_2.jpg)"></div>
                                     <div class="pl-3 tx">
-                                        <p class="name">Roger Scott</p>
-                                        <span class="position">Marketing Manager</span>
+                                        <p class="name">HISTORICAL</p>
+                                        <span class="position">of Stainaa</span>
                                     </div>
                                 </div>
                             </div>
@@ -62,27 +63,12 @@ Tentang
                         <div class="testimony-wrap">
                             <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-quote-left"></div>
                             <div class="text">
-                                <p class="mb-4 msg">VISI</p>
-                                <div class="d-flex align-items-center">
-                                    <div class="user-img" style="background-image: url(plugin/images/person_1.jpg)"></div>
-                                    <div class="pl-3 tx">
-                                        <p class="name">Roger Scott</p>
-                                        <span class="position">Marketing Manager</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="testimony-wrap">
-                            <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-quote-left"></div>
-                            <div class="text">
-                                <p class="mb-4 msg">MISI</p>
+                                <p class="mb-4 msg">PROFIL PIMPINAN</p>
                                 <div class="d-flex align-items-center">
                                     <div class="user-img" style="background-image: url(plugin/images/person_2.jpg)"></div>
                                     <div class="pl-3 tx">
-                                        <p class="name">Roger Scott</p>
-                                        <span class="position">Marketing Manager</span>
+                                        <p class="name">LEADERS</p>
+                                        <span class="position">of Stainaa</span>
                                     </div>
                                 </div>
                             </div>
