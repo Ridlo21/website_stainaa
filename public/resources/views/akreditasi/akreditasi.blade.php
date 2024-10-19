aa19a59bd9ff95937e5c3092bc53e70e838a858e
oles <muklek99@gmail.com>
2024-10-20 00:26:16 +0700

    ok

--- public/resources/views/akreditasi/akreditasi.blade.php
+++ public/resources/views/akreditasi/akreditasi.blade.php
@@ -31,9 +31,33 @@ Akreditasi
             </div>
         <div class="row g-xl-5">
             <div class="col-md-12 heading-section" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000">
-                <span class="subheading">About Us</span>
-                <!-- <h2 class="mb-4">Oakberry A Real Estate Company</h2> -->
-                <p>BEM adalah singkatan dari Badan Eksekutif Mahasiswa, yaitu organisasi mahasiswa intra kampus yang bertugas sebagai lembaga eksekutif di tingkat universitas, institut, atau sekolah tinggi. BEM biasanya dipimpin oleh seorang ketua dan wakil ketua.</p>
+            
+        <table class="table table-bordered" style="width:100%" border>
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Kategori</th>
+                        <th>No Butir</th>
+                        <th>Nama Dokument</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>APT</td>
+                        <td>C.2</td>
+                        <td>
+                            1 . <a href="https://docs.google.com/document/d/1E_GMoLizWIexcUBrT36xR6hGpQE6MDeP/edit?usp=drive_link&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Laporan Capaian Kinerja Tahunan</a><br>
+                            2 . <a href="https://docs.google.com/document/d/1KNmqFylcV1dWvm9XFUuy3MxuXKAmNzr5/edit?usp=drive_link&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Kebijakan SPMI STAINAA</a><br>
+                            3 . <a href="https://docs.google.com/document/d/1sb2xtu2igrsd_VG9sVIGtnCRXC7eyt97/edit?usp=drive_link&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">RTM STAINAA</a><br>
+                            4 . <a href="https://docs.google.com/document/d/1QdaeiGNietilGegDn4a_7rBLq_ezJY_2/edit?usp=sharing&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Jejaring Dengan Mitra</a><br>
+                            5 . <a href="https://docs.google.com/document/d/1XDYJSHpcWnYD8tcCQ-XCGLrqRY-nlUJx/edit?usp=sharing&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Pedoman Pengelolaan</a><br>
+                            6 . <a href="https://docs.google.com/document/d/1pT323_fUpy3untfqCCCVQzXgUfYAkt7q/edit?usp=sharing&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Pedoman Kerjasama</a><br>
+                            7 . <a href="https://shinigami.ae/" target="blank">Makalah Arsiterktur Database</a><br>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
             </div>
         </div>
     </div>
@@ -46,24 +70,70 @@ Akreditasi
             </div>
         <div class="row g-xl-5">
             <div class="col-md-12 heading-section" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000">
-                <span class="subheading">About Us</span>
-                <!-- <h2 class="mb-4">Oakberry A Real Estate Company</h2> -->
-                <p>BEM adalah singkatan dari Badan Eksekutif Mahasiswa, yaitu organisasi mahasiswa intra kampus yang bertugas sebagai lembaga eksekutif di tingkat universitas, institut, atau sekolah tinggi. BEM biasanya dipimpin oleh seorang ketua dan wakil ketua.</p>
+                <table class="table table-bordered" style="width:100%" border>
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Kategori</th>
+                            <th>No Butir</th>
+                            <th>Nama Dokument</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>APT</td>
+                            <td>C.2</td>
+                            <td>
+                                1 . <a href="https://docs.google.com/document/d/1E_GMoLizWIexcUBrT36xR6hGpQE6MDeP/edit?usp=drive_link&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Laporan Capaian Kinerja Tahunan</a><br>
+                                2 . <a href="https://docs.google.com/document/d/1KNmqFylcV1dWvm9XFUuy3MxuXKAmNzr5/edit?usp=drive_link&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Kebijakan SPMI STAINAA</a><br>
+                                3 . <a href="https://docs.google.com/document/d/1sb2xtu2igrsd_VG9sVIGtnCRXC7eyt97/edit?usp=drive_link&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">RTM STAINAA</a><br>
+                                4 . <a href="https://docs.google.com/document/d/1QdaeiGNietilGegDn4a_7rBLq_ezJY_2/edit?usp=sharing&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Jejaring Dengan Mitra</a><br>
+                                5 . <a href="https://docs.google.com/document/d/1XDYJSHpcWnYD8tcCQ-XCGLrqRY-nlUJx/edit?usp=sharing&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Pedoman Pengelolaan</a><br>
+                                6 . <a href="https://docs.google.com/document/d/1pT323_fUpy3untfqCCCVQzXgUfYAkt7q/edit?usp=sharing&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Pedoman Kerjasama</a><br>
+                                7 . <a href="https://shinigami.ae/" target="blank">Makalah Arsiterktur Database</a><br>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
 </section>
-<section class="ftco-section ftco-about-section ftco-no-pb">
+<section class="ftco-section ftco-about-section ftco-no-pb ">
     <div class="container-xl">
         <div class="col-md-12 heading-section" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000">
-                <h2 class="mb-4">PAI</h2>
-                <hr style="border: 3px solid blue;">
-            </div>
+            <h2 class="mb-4">PAI</h2>
+            <hr style="border: 3px solid blue;">
+        </div>
         <div class="row g-xl-5">
-            <div class="col-md-12 heading-section" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000">
-                <span class="subheading">About Us</span>
-                <!-- <h2 class="mb-4">Oakberry A Real Estate Company</h2> -->
-                <p>BEM adalah singkatan dari Badan Eksekutif Mahasiswa, yaitu organisasi mahasiswa intra kampus yang bertugas sebagai lembaga eksekutif di tingkat universitas, institut, atau sekolah tinggi. BEM biasanya dipimpin oleh seorang ketua dan wakil ketua.</p>
+        <div class="col-md-12 heading-section" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000">
+                <table class="table table-bordered" style="width:100%" border>
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Kategori</th>
+                            <th>No Butir</th>
+                            <th>Nama Dokument</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>APT</td>
+                            <td>C.2</td>
+                            <td>
+                                1 . <a href="https://docs.google.com/document/d/1E_GMoLizWIexcUBrT36xR6hGpQE6MDeP/edit?usp=drive_link&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Laporan Capaian Kinerja Tahunan</a><br>
+                                2 . <a href="https://docs.google.com/document/d/1KNmqFylcV1dWvm9XFUuy3MxuXKAmNzr5/edit?usp=drive_link&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Kebijakan SPMI STAINAA</a><br>
+                                3 . <a href="https://docs.google.com/document/d/1sb2xtu2igrsd_VG9sVIGtnCRXC7eyt97/edit?usp=drive_link&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">RTM STAINAA</a><br>
+                                4 . <a href="https://docs.google.com/document/d/1QdaeiGNietilGegDn4a_7rBLq_ezJY_2/edit?usp=sharing&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Jejaring Dengan Mitra</a><br>
+                                5 . <a href="https://docs.google.com/document/d/1XDYJSHpcWnYD8tcCQ-XCGLrqRY-nlUJx/edit?usp=sharing&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Pedoman Pengelolaan</a><br>
+                                6 . <a href="https://docs.google.com/document/d/1pT323_fUpy3untfqCCCVQzXgUfYAkt7q/edit?usp=sharing&ouid=101588959234804921917&rtpof=true&sd=true" target="blank">Pedoman Kerjasama</a><br>
+                                7 . <a href="https://shinigami.ae/" target="blank">Makalah Arsiterktur Database</a><br>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
